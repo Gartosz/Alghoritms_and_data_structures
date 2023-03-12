969bec94b37c24b3172e90d2565c2e0624f3fb5c
Bartosz <bartekg001@gmail.com>
2023-03-12 23:31:49 +0100

    Implement and test inorder_do recursive function.

--- Lista_2/programming/z1.cpp
+++ Lista_2/programming/z1.cpp
@@ -25,9 +25,22 @@ void insert(Node *&t, int x)
     *current_node = new Node(x, *parent_node);
 }
 
+void inorder_do_rec(Node *t, void f(Node*))
+{
+    if (!t)
+        return;
+    inorder_do_rec(t->left, f);
+    f(t);
+    inorder_do_rec(t->right, f);
+}
+
 int main()
 {
     Node *bst = new Node(30);
     insert(bst, 10);
     std::cout << bst->x << " " << bst->left->x << std::endl;
+    int nodes[6] = {1, 5, 7, 24, 73, 39};
+    for (int i = 0; i < sizeof(nodes)/sizeof(*nodes); ++i)
+        insert(bst, nodes[i]);
+    inorder_do_rec(bst, [](Node *t) {std::cout << t->x << std::endl;});
 }
