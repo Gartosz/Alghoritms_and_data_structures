7c90081eb5822837191c26c7efe1b0d8a6069c04
Bartosz <bartekg001@gmail.com>
2023-03-19 22:50:14 +0100

    Rename class and struct to describe what they are.

--- Lista_2/programming/z4.cpp
+++ Lista_2/programming/z4.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
 
-class BSTiter
+class Node
 {
     public:
     int x;
-    BSTiter *parent;
-    BSTiter *left;
-    BSTiter *right;
+    Node *parent;
+    Node *left;
+    Node *right;
 
-    BSTiter(int key, BSTiter *_parent = nullptr) : x(key), parent(_parent), right(nullptr), left(nullptr) {} 
+    Node(int key, Node *_parent = nullptr) : x(key), parent(_parent), right(nullptr), left(nullptr) {} 
 
-    struct Iterator 
+    struct BSTiter 
     {
     private:
-        BSTiter *current;
+        Node *current;
     };
 
     
     void insert(int x)
     {
-        BSTiter *parent_node = this;
-        BSTiter **current_node = &parent_node;
+        Node *parent_node = this;
+        Node **current_node = &parent_node;
         while(*current_node)
         {
             parent_node = *current_node;
             current_node = x < (*current_node)->x ? &((*current_node)->left) : &((*current_node)->right);
         }
-        *current_node = new BSTiter(x, parent_node);
+        *current_node = new Node(x, parent_node);
     }
 
 };
 
 int main()
 {
-    BSTiter tree(41);
+    Node tree(41);
     tree.insert(16);
     std::cout << tree.x << " " << (*tree.left).x;
     return 0;
