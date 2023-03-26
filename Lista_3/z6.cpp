5c068a85a15ae0c9bf649862c28225ae34b961bb
Bartosz <bartekg001@gmail.com>
2023-03-26 22:18:44 +0200

    Add code from previous list.

---
+++ Lista_3/z6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class Node
+{
+    public:
+    int x;
+    Node *parent;
+    Node *left;
+    Node *right;
+    
+    Node(int key, Node *_parent = nullptr) : x(key), parent(_parent), right(nullptr), left(nullptr) {} 
+    
+    void insert(int x)
+    {
+        Node *parent_node = this;
+        Node **current_node = &parent_node;
+        while(*current_node)
+        {
+            parent_node = *current_node;
+            current_node = x < (*current_node)->x ? &((*current_node)->left) : &((*current_node)->right);
+        }
+        *current_node = new Node(x, parent_node);
+    }
+
+
+};
+
+int main ()
+{
+    Node tree(41);
+    int nodes[11] = {16, 3, 4, 9, 56, 18, 37, 65, 58, 2, 1};
+    for (int i = 0; i < sizeof(nodes)/sizeof(*nodes); ++i)
+        tree.insert(nodes[i]);
+}
