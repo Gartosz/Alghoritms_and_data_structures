44b902e897aafe19dd9ad7ff1c0b62ec560effd9
Bartosz <bartekg001@gmail.com>
2023-03-27 01:10:02 +0200

    Implement getting pointer to node with nth index.

--- Lista_3/z6.cpp
+++ Lista_3/z6.cpp
@@ -30,10 +30,26 @@ class Node
                 right = new Node(key, this);
         }
     }
+
+    Node *nth(int n)
+    {
+        if (n < 0)
+            return nullptr;
+        Node *node = this;
+        while(node)
+        {
+            if (node->nL == n)
+                return node;
+            else if (node->nL <= n)
+            {
+                n -= 1 + node->nL;
+                node = node->right;
             }
-            else if(right)
-                right->insert(key);
+            else
+                node = node->left;
         }
+        
+        return nullptr;
     }
 
 };
@@ -43,8 +59,12 @@ int main ()
     Node tree(41);
     std::cout << tree.nL << " ";
     tree.insert(16);
-    std::cout << tree.nL << "\n";
+    std::cout << tree.nL << " ";
     int nodes[10] = {3, 4, 9, 56, 18, 37, 65, 58, 2, 1};
     for (int i = 0; i < sizeof(nodes)/sizeof(*nodes); ++i)
         tree.insert(nodes[i]);
+    std::cout << tree.nL << "\n";
+    std::cout << tree.nth(7)->x << "\n";
+    std::cout << tree.nth(2)->x << "\n";
+    std::cout << tree.nth(10)->x;
 }
