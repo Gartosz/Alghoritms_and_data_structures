159ee227f5d418a9564bf2419087091f695d5a62
Bartosz <bartekg001@gmail.com>
2023-04-20 10:56:15 +0200

    Dectare kth function, change type in partition to int.

--- Lista_5/z9.cpp
+++ Lista_5/z9.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 
 
-int partition (double t[], int n)
+int partition (int t[], int n)
 {
-  int k = -1;
-  double x = t[n / 2]; // pivot bierzemy posrodku
-  for(;;)
-  {
-     do ++k; while (t[k] < x);
-     do --n; while (t[n] > x);
-     if (k < n) std::swap(t[k],t[n]);
-     else       return k;
-  }
+    int k = -1;
+    double x = t[n / 2];
+    for(;;)
+    {
+        do ++k; 
+        while (t[k] < x);
+        do --n; 
+        while (t[n] > x);
+
+        if (k < n) 
+            std::swap(t[k],t[n]);
+        else       
+            return k;
+    }
+}
+
+int kth(int t[], int n)
+{
+
 }
 
 int main()
 {
-    
+
 }
